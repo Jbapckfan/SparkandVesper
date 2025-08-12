4cdce8681f6c799c60eb22a40cf1baebed147149
James Alford <james.alford@gmail.com>
2025-08-12 08:32:24 -1000

    Initial Commit

---
+++ Spark&VesperTests/Spark_VesperTests.swift
@@ -0,0 +1,17 @@
+//
+//  Spark_VesperTests.swift
+//  Spark&VesperTests
+//
+//  Created by James Alford on 8/12/25.
+//
+
+import Testing
+@testable import Spark_Vesper
+
+struct Spark_VesperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Spark&VesperUITests/Spark_VesperUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Spark_VesperUITestsLaunchTests.swift
+//  Spark&VesperUITests
+//
+//  Created by James Alford on 8/12/25.
+//
+
+import XCTest
+
+final class Spark_VesperUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
